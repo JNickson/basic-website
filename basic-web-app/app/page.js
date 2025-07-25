34c4155c1ec4a3bad509d86db12d3bbf54a7a7b0
JNickson <jack.nickson@btinternet.com>
2025-07-25 11:16:28 +0100

    fix fmt

--- basic-web-app/app/page.js
+++ basic-web-app/app/page.js
@@ -9,14 +9,12 @@ export default function Home() {
     <main style={{ padding: 20 }}>
       <h1 className={styles.title}> Welcome </h1>
       <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}> Add 1 to count</button>
-      <img
-        src="/logo.jpg"
-        alt="Logo"
-        width={100}
-        height={100}
-        style={{ marginTop: 20 }}
-      />
+      <div>
+        <button onClick={() => setCount(count + 1)}> Add 1 to count</button>
+      </div>
+      <div style={{ marginTop: 20 }}>
+        <img src="/logo.jpg" alt="Logo" width={100} height={100} />
+      </div>
     </main>
   );
 }
